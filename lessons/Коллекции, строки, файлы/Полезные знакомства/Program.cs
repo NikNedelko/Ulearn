634fdcfb89410beb5e075a520a420cee11b955e3
Nikita <nedelko1nik@gmail.com>
2021-12-02 13:27:49 +0300

    first commit

---
+++ lessons/Коллекции, строки, файлы/Полезные знакомства/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+/// <summary>
+/// https://ulearn.me/course/basicprogramming/Poleznye_znakomstva_9a4d5ff4-2331-4bef-a3ee-88e54bec88c8
+/// </summary>
+namespace Полезные_знакомства
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sasha = new List<string>();
+            sasha.Add("A:А@A.A");
+            sasha.Add("Ваня:v@mail.ru");
+            sasha.Add("Вася:vasiliy@gmail.com");
+            sasha.Add("Ваня:ivan@grozniy.ru");
+            sasha.Add("Саша:sasha1995@sasha.ru");
+            sasha.Add("Саша:alex@nd.ru");
+            sasha.Add("Паша:pavel.egorov@urfu.ru");
+            sasha.Add("Юрий:dolg@rukiy.ru");
+            sasha.Add("Гена:genadiy.the.best@inbox.ru");
+            OptimizeContacts(sasha);
+        }
+
+        private static Dictionary<string, List<string>> OptimizeContacts(List<string> contacts)
+        {
+            var dictionary = new Dictionary<string, List<string>>();
+            string[] user = new string[2];
+            foreach (var it in contacts)
+            {
+                user = it.Split(":");
+                string shortName;
+                if (user[0].Length < 2)
+                    shortName = user[0];
+                else
+                shortName=user[0].Substring(0,2);
+                if (!dictionary.ContainsKey(shortName))
+                {
+                    dictionary.Add(shortName, new List<string>());
+                    dictionary[shortName].Add(it);
+                }
+                else
+                    dictionary[shortName].Add(it);
+            }
+            return dictionary;
+        }
+    }
+}
---
+++ lessons/Коллекции, строки, файлы/Шифр незнакомки/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Шифр_незнакомки
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] letter = {"решИла нЕ Упрощать и зашифРОВАтЬ Все послаНИЕ",
+            "дАже не Старайся нИЧЕГО у тЕбя нЕ получится с расшифРОВкой",
+            "Сдавайся НЕ твоего ума Ты не споСОбЕн Но может быть",
+            "если особенно упорно подойдешь к делу",
+            "будет Трудно конечнО",
+            "Код ведЬ не из простых",
+            "очень ХОРОШИЙ код",
+            "то у тебя все получится",
+            "и я буДу Писать тЕбЕ еще",
+            "чао",
+            };
+        }
+
+        private static string DecodeMessage(string[] lines)
+        {
+            List<string> selecter = new List<string>();
+            string result = "", helper = "", word = "";
+            for (int i = 0; i < lines.Length; i++)
+            {
+                helper = lines[i];
+                for (int j = 0; j < helper.Length; j++)
+                {
+                    if (helper == " ")
+                    {
+                        continue;
+                    }
+                    if (helper[j] == char.ToUpper(helper[j]) && j == 0 || helper[j] == char.ToUpper(helper[j]) && helper[j - 1] == ' ')
+                    {
+                        int counter = j;
+                        char[] convertedHelper = helper.ToCharArray();
+                        //helper[counter] != ' '
+                        while (true)
+                        {
+                            if (convertedHelper[counter] == ' ' || counter == helper.Length - 1)
+                            {
+                                break;
+                            }
+                            word += convertedHelper[counter];
+                            counter++;
+                        }
+                        word += " ";
+                        selecter.Add(word);
+                        word = "";
+                    }
+                }
+            }
+            for (int i = selecter.Count; i > 0; i--)
+            {
+                result += selecter[i - 1];
+            }
+            return result;
+        }
+    }
+
+}
+/*
+List<string> selecter = new List<string>();
+            string result = "", helper="";
+            for (int i = 0; i < lines.Length; i++)
+            {
+                helper = lines[i];
+                if (helper==" ")
+                {
+                    continue;
+                }
+                if (helper[0]==char.ToUpper(helper[0]))
+                {
+                    selecter.Add(helper);
+                }
+            }
+           for (int i = selecter.Count; i >0; i--)
+           {
+               result+= selecter[i-1];
+           }
+            return result;
+
+
+            string[] letter = {"решИла ", "нЕ ", "Упрощать ", "и ", "зашифРОВАтЬ ", "Все ", "послаНИЕ ", 
+             "дАже ", "не ", "Старайся ", "нИЧЕГО ", "у ", "тЕбя ", "нЕ ", "получится ", "с ", "расшифРОВкой ",
+            "Сдавайся ", "НЕ ", "твоего ", "ума ", "Ты ", "не ", "споСОбЕн ", "Но ", "может ", "быть ",
+            "если ", "особенно ", "упорно ", "подойдешь ", "к ", "делу ",
+
+            "будет ", "Трудно ", "конечнО ",
+            "Код ", "ведЬ ", "не ", "из ", "простых ",
+            "очень ", "ХОРОШИЙ ", "код ",
+            "то ", "у ", "тебя ", "все ", "получится ",
+            "и ", "я ", "буДу ", "Писать ", "тЕбЕ ", "еще"
+
+            +"чао"};
+*/
