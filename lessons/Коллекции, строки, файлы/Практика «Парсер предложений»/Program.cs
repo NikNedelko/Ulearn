5445a09f66191556ee5bce5f31ef87407afc2002
Nikita <nedelko1nik@gmail.com>
2021-12-03 16:41:10 +0300

    new tasks

---
+++ lessons/Коллекции, строки, файлы/Практика «Парсер предложений»/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Практика__Парсер_предложений_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ lessons/Коллекции, строки, файлы/Снова незнакомка/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+
+namespace Снова_незнакомка
+{
+    /// <summary>
+    /// https://ulearn.me/course/basicprogramming/Snova_neznakomka_741d39bd-d543-40d2-abbc-941c7f778106
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var text = new string[] {"push Привет! Это снова я! Пока!"
+            ,"pop 5",
+            "push Как твои успехи? Плохо?",
+            "push qwertyuiop",
+            "push 1234567890",
+            "pop 26"};
+            /// ....
+            ApplyCommands(text);
+        }
+
+        private static string ApplyCommands(string[] commands)
+        {
+            var builder = new StringBuilder();
+            string result = "";
+            for (int i = 0; i < commands.Length; i++)
+            {
+                if (commands[i].Substring(0, 4) == "push")
+                    builder.Append(commands[i].Substring(5));
+                else
+                    builder.Remove(builder.Length - Convert.ToInt32(commands[i].Substring(4)),
+                    Convert.ToInt32(commands[i].Substring(4)));
+            }
+
+            return result=builder.ToString();
+        }
+
+    }
+}
