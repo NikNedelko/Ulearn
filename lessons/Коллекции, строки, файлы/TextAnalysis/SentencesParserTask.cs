2a76bb226d8e8adcee90b22653663ac38ed1277f
Nikita <nedelko1nik@gmail.com>
2021-12-04 13:31:14 +0300

    add a first of three parts proj

---
+++ lessons/Коллекции, строки, файлы/TextAnalysis/SentencesParserTask.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace TextAnalysis
+{
+    /// <summary>
+    /// https://ulearn.me/course/basicprogramming/Praktika_Parser_predlozheniy__d41a27ad-a377-4fbd-ba1c-1bd761c69dd1
+    /// </summary>
+    static class SentencesParserTask
+    {
+        
+            public static List<List<string>> ParseSentences(string text)
+            {
+                var listSentences = new List<List<string>>(); // Возвращаемый список
+                if (text == null) return null; //проверка на наличие текста
+                text = text.ToLower();
+                var sentences = text.Split(".:;?!()".ToCharArray(),//парсеный на предложения текст
+                    StringSplitOptions.RemoveEmptyEntries);
+                foreach (var sent in sentences)
+                {
+                    var listWords = new List<string>(); // Список для слов
+                    var builder = new StringBuilder();
+                    foreach (var ch in sent)
+                    {
+                        if (char.IsLetter(ch) || ch == '\'')
+                            builder.Append(ch);
+                        else
+                            AddNotEmptyWord(builder, listWords);
+                    }
+                    AddNotEmptyWord(builder, listWords);
+                    if (listWords.Count > 0)
+                        listSentences.Add(listWords);
+                }
+                return listSentences;
+            }
+
+            public static void AddNotEmptyWord(StringBuilder builder, List<string> listWords)
+            {
+                if (builder.Length > 0)
+                {
+                    listWords.Add(builder.ToString());
+                    builder.Clear();
+                }
+            }
+
+        }
+}
