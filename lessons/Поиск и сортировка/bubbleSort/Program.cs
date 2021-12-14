97b36e051a3a6b801f9005daad936721cecd08ba
Nikita <nedelko1nik@gmail.com>
2021-12-14 22:33:26 +0300

    new new new lessons

---
+++ lessons/Поиск и сортировка/bubbleSort/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace bubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+        private static void BubbleSort(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+                for (int j = 0; j < array.Length - 1; j++)
+                    if (array[j] > array[j + 1])
+                    {
+                        int t = array[j + 1];
+                        array[j + 1] = array[j];
+                        array[j] = t;
+                    }
+        }
+    }
+}
